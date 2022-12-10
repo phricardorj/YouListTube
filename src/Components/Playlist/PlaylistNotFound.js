dbf9a640d7e3f945f47f470257e93fc62c71f9c2
Pedro Ricardo <brpedroricardo@gmail.com>
2022-12-10 20:13:13 -0300

    fix: responsive mobile design

--- src/Components/Playlist/PlaylistNotFound.js
+++ src/Components/Playlist/PlaylistNotFound.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./PlaylistNotFound.module.css";
+import infoCode from "../../Assets/infoCode.png";
 
 const PlaylistNotFound = () => {
   return (
     <div className={styles.error}>
-      <h1>Ops 💥! Playlist não encontrada!</h1>
-      <p>Verifique o código da Playlist informado e tente novamente.</p>
-      <Link to="/" className={styles.voltar}>
-        Voltar
-      </Link>
+      <div className={styles.container}>
+        <h1>Ops 💥! Playlist não encontrada!</h1>
+        <p>Verifique o código da Playlist informado e tente novamente.</p>
+        <img
+          src={infoCode}
+          alt="Imagem com informação de como achar o código da Playlist"
+          className={styles.infoCode}
+        />
+        <Link to="/" className={`btn-default ${styles.btn}`}>
+          Voltar
+        </Link>
+      </div>
     </div>
   );
 };
